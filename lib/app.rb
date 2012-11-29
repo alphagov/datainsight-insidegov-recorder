cba4560c918cd785abbdce0e93a48c290d8f4ba1
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-11-29 16:44:22 +0000

    Config for logging and airbrake

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,22 @@
 require "bundler/setup"
 Bundler.require(:default, :exposer)
-require "model/weekly_reach"
+
+require_relative "model/weekly_reach"
+require_relative "datamapper_config"
+require_relative "initializers"
+
+helpers Datainsight::Logging::Helpers
+
+use Airbrake::Rack
+enable :raise_errors
+
+configure do
+  enable :logging
+  unless test?
+    Datainsight::Logging.configure(:type => :exposer)
+    DataMapperConfig.configure
+  end
+end
 
 get "/visitors/weekly" do
   WeeklyReach.json_representation
