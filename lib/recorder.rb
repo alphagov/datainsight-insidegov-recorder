9a9ead88eda01dc93e8b946e25cfc136c79cfb62
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-03 18:55:11 +0000

    Pull recorder code up into the gem.

--- lib/recorder.rb
+++ lib/recorder.rb
@@ -2,45 +2,18 @@ require "json"
 
 require "bundler/setup"
 Bundler.require(:default, :recorder)
+require "datainsight_recorder/recorder"
 
 require_relative "model/weekly_reach"
 
 class Recorder
-  ROUTING_KEYS = ["google_analytics.inside_gov.visitors.weekly"]
+  include DataInsight::Recorder::AMQP
 
-  def run
-    queue.subscribe do |msg|
-      begin
-        logger.debug { "Received a message: #{msg}" }
-        WeeklyReach.update_from_message(parse_amqp_message(msg))
-      rescue Exception => e
-        logger.error { e }
-      end
-    end
+  def routing_keys
+    ["google_analytics.inside_gov.visitors.weekly"]
   end
 
-  private
-  def queue
-    @queue ||= create_queue
-  end
-
-  def create_queue
-    client = Bunny.new ENV["AMQP"]
-    client.start
-    queue = client.queue(ENV["QUEUE"] || "insidegov")
-    exchange = client.exchange("datainsight", :type => :topic)
-
-    ROUTING_KEYS.each do |key|
-      queue.bind(exchange, :key => key)
-      logger.info("Bound to #{key}, listening for events")
-    end
-
-    queue
-  end
-
-  def parse_amqp_message(raw_message)
-    message = JSON.parse(raw_message[:payload], :symbolize_names => true)
-    message[:envelope][:_routing_key] = raw_message[:delivery_details][:routing_key]
-    message
+  def update_message(message)
+    WeeklyReach.update_from_message(message)
   end
 end
---
+++ spec/recorder_spec.rb
@@ -0,0 +1,40 @@
+require_relative "../spec_helper"
+require_relative "../../lib/recorder"
+require_relative "../../lib/model/weekly_reach"
+require "datainsight_recorder/test_helpers"
+
+describe "Recorder" do
+
+  it "should listen to the correct topic" do
+    should_listen_to_topics(
+      "google_analytics.inside_gov.visitors.weekly"
+    )
+
+    Recorder.new.run
+  end
+
+  it "should send message to correct model" do
+    recorder = Recorder.new
+    queue = mock()
+    recorder.stub(:queue).and_return(queue)
+
+    amqp_message = {
+      delivery_details: {
+        routing_key: "foo"
+      },
+      payload: '{"envelope":{}}'
+    }
+
+    parsed_message = {
+      envelope: {
+        _routing_key: "foo"
+      }
+    }
+
+    queue.should_receive(:subscribe).and_yield(amqp_message)
+
+    WeeklyReach.should_receive(:update_from_message).with(parsed_message)
+
+    recorder.run
+  end
+end
