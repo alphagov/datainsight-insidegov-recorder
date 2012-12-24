efdf72b0ca9feb464d86465d8cedceb546244053
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-24 18:13:41 +0000

    Add tests for the policy / policy_entries join
    Also rename the policy factory file.

--- spec/model/policy_entries_spec.rb
+++ spec/model/policy_entries_spec.rb
@@ -1,44 +1,46 @@
 require "spec_helper"
 
 describe "the policy_entries model" do
-  it "should validate entries correctly (only integers)" do
-    policy_visits_record = PolicyEntries.new(entries: "test")
+  describe "validation" do
+    it "should validate entries correctly (only integers)" do
+      policy_visits_record = PolicyEntries.new(entries: "test")
 
-    policy_visits_record.valid?.should == false
+      policy_visits_record.valid?.should == false
 
-    policy_visits_record.errors[:entries].first.should == "Entries must be an integer"
-  end
+      policy_visits_record.errors[:entries].first.should == "Entries must be an integer"
+    end
 
-  it "should validate entries correctly (required field)" do
-    policy_visits_record = PolicyEntries.new(slug: "test")
+    it "should validate entries correctly (required field)" do
+      policy_visits_record = PolicyEntries.new(slug: "test")
 
-    policy_visits_record.valid?.should == false
+      policy_visits_record.valid?.should == false
 
-    policy_visits_record.errors[:entries].first.should == "Entries must not be blank"
-  end
+      policy_visits_record.errors[:entries].first.should == "Entries must not be blank"
+    end
 
-  it "should validate entries correctly (positive)" do
-    policy_visits_record = PolicyEntries.new(entries: -6, slug: "foobar")
+    it "should validate entries correctly (positive)" do
+      policy_visits_record = PolicyEntries.new(entries: -6, slug: "foobar")
 
-    policy_visits_record.valid?.should == false
+      policy_visits_record.valid?.should == false
 
-    policy_visits_record.errors[:entries].first.should == "It must be greater than or equal to 0"
-  end
+      policy_visits_record.errors[:entries].first.should == "It must be greater than or equal to 0"
+    end
 
-  it "should validate slug correctly (required field)" do
-    policy_visits_record = PolicyEntries.new(entries: 1)
+    it "should validate slug correctly (required field)" do
+      policy_visits_record = PolicyEntries.new(entries: 1)
 
-    policy_visits_record.valid?.should == false
+      policy_visits_record.valid?.should == false
 
-    policy_visits_record.errors[:slug].first.should == "Slug must not be blank"
-  end
+      policy_visits_record.errors[:slug].first.should == "Slug must not be blank"
+    end
 
-  it "should return know if it has metadata" do
-    policy_visits_record = PolicyEntries.new(entries: 1, slug: "foo", policy: {})
-    policy_visits_record_with_no_metadata = PolicyEntries.new(entries: 1, slug: "foo", policy: nil)
+    it "should return know if it has metadata" do
+      policy_visits_record = PolicyEntries.new(entries: 1, slug: "foo", policy: {})
+      policy_visits_record_with_no_metadata = PolicyEntries.new(entries: 1, slug: "foo", policy: nil)
 
-    policy_visits_record.has_metadata?.should == true
-    policy_visits_record_with_no_metadata.has_metadata?.should == false
+      policy_visits_record.has_metadata?.should == true
+      policy_visits_record_with_no_metadata.has_metadata?.should == false
+    end
   end
 
   describe "top" do
@@ -59,4 +61,22 @@ describe "the policy_entries model" do
     end
   end
 
+  describe "policy join" do
+    before(:each) do
+      FactoryGirl.create :policy_entries, slug: "/my-slug"
+      FactoryGirl.create :policy, slug: "/my-slug"
+    end
+
+    it "should return the joined policy" do
+      policy_entries = PolicyEntries.first
+      policy_entries.policy.should be_a(Policy)
+      policy_entries.policy.slug.should == "/my-slug"
+    end
+
+    it "should return nil if no policy is" do
+      Policy.destroy
+      policy_entries = PolicyEntries.first
+      policy_entries.policy.should be_nil
+    end
+  end
 end
