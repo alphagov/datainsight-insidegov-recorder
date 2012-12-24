3833be23c979f783785e76c2bd4b18f02f945271
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-24 18:01:17 +0000

    Add more specific recorder tests.
    Test that messages get sent to the correct models.

--- spec/recorders/recorder_spec.rb
+++ spec/recorders/recorder_spec.rb
@@ -3,6 +3,9 @@ require_relative "../../lib/recorders/recorder"
 require "datainsight_recorder/test_helpers"
 
 describe Recorder do
+  before(:each) do
+    @recorder = Recorder.new
+  end
   it "should listen to the correct topic" do
     should_listen_to_topics(
       "google_analytics.insidegov.policy_entries.weekly",
@@ -11,7 +14,7 @@ describe Recorder do
       "inside_gov.policies"
     )
 
-    Recorder.new.run
+    @recorder.run
   end
 
   it "should be able to process messages for registered routing keys" do
@@ -20,11 +23,30 @@ describe Recorder do
     FormatVisits.should_receive(:update_from_message)
     Policy.should_receive(:update_from_message)
 
-    recorder = Recorder.new
-    recorder.routing_keys.each do |key|
+    @recorder.routing_keys.each do |key|
       expect {
-        recorder.update_message(envelope: {_routing_key: key})
+        @recorder.update_message(envelope: {_routing_key: key})
       }.to_not raise_error
     end
+  end
+
+  it "should send weekly reach messages to the WeeklyReach model" do
+    WeeklyReach.should_receive(:update_from_message)
+    @recorder.update_message(envelope: {_routing_key: "google_analytics.inside_gov.visitors.weekly"})
+  end
+
+  it "should send entry and success messages to the FormatVisits model" do
+    FormatVisits.should_receive(:update_from_message)
+    @recorder.update_message(envelope: {_routing_key: "google_analytics.insidegov.entry_and_success.weekly"})
+  end
+
+  it "should send policy entry messages to the PolicyEntries model" do
+    PolicyEntries.should_receive(:update_from_message)
+    @recorder.update_message(envelope: {_routing_key: "google_analytics.insidegov.policy_entries.weekly"})
+  end
+
+  it "should send policy messages to the Policy model" do
+    Policy.should_receive(:update_from_message)
+    @recorder.update_message(envelope: {_routing_key: "inside_gov.policies"})
   end
 end
