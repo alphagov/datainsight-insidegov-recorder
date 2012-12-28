a913bb81e50b0e3d88381010f60187b1103b67f7
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-28 14:30:10 +0000

    Correct migration number for removing department from policy

--- db/migrate/remove_department_from_policy.rb
+++ db/migrate/remove_department_from_policy.rb
@@ -1,7 +1,7 @@
 require "data_mapper"
 require "dm-migrations/migration_runner"
 
-migration 1, :remove_department_from_policy do
+migration 2, :remove_department_from_policy do
   up do
     modify_table :policies do
       drop_column :department
