9303a672e7c41b3b85a02dcfbd139ef6244a3469
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-01-04 12:20:29 +0000

    Perform migrations only if required.
    @pbadenski
    @robyoung

--- db/migrate/remove_department_from_policy.rb
+++ db/migrate/remove_department_from_policy.rb
@@ -4,7 +4,9 @@ require "dm-migrations/migration_runner"
 migration 2, :remove_department_from_policy do
   up do
     modify_table :policies do
-      drop_column :department
+      if adapter.field_exists?("policies", "department")
+        drop_column :department
+      end
     end
   end
 end
--- db/migrate/update_policy_model.rb
+++ db/migrate/update_policy_model.rb
@@ -5,8 +5,14 @@ migration 1, :update_policy_model do
   up do
     modify_table :policies do
       change_column :department, "TEXT", allow_nil: false
-      add_column :organisations, "TEXT", allow_nil: false
-      add_column :policy_updated_at, DateTime, allow_nil: false
+
+      unless adapter.field_exists?("policies", "organisations")
+        add_column :organisations, "TEXT", allow_nil: false
+      end
+
+      unless adapter.field_exists?("policies", "policy_updated_at")
+        add_column :policy_updated_at, DateTime, allow_nil: false
+      end
     end
   end
 end
