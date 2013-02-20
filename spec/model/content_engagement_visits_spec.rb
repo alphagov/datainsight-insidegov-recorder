182dd1791cf5e3cfddf1627f7f1371f5312134e4
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-20 15:50:35 +0000

    Add tests for ContentEngagementVisits.update_from_message
    @pbadenski
    @robyoung

--- spec/model/content_engagement_visits_spec.rb
+++ spec/model/content_engagement_visits_spec.rb
@@ -1,3 +1,6 @@
+require_relative "../spec_helper"
+require_relative "../../lib/model/content_engagement_visits"
+
 describe ContentEngagementVisits do
   describe "last_week_visits" do
     it "should return visits for the available week" do
@@ -13,6 +16,75 @@ describe ContentEngagementVisits do
 
       content_engagement_visits.should have(2).items
       content_engagement_visits.should_not include(older_item)
+    end
+
+    it "should return engagement together with artefact details"
+    it "should not return engagement that does not have a matching artefact"
+    it "should return artefacts even if they do not have matching engagement"
+
+    it "should not return artefacts that have been disabled"
+  end
+
+  describe "update_from_message" do
+    before(:each) do
+      @message = {
+        :envelope => {
+          :collected_at => DateTime.new(2013, 2, 27, 11, 11, 0, 0).strftime,
+          :collector    => "Google Analytics",
+          :_routing_key => "google_analytics.insidegov.content_engagement.weekly",
+        },
+        :payload => {
+          :start_at => DateTime.new(2013, 2, 18, 0, 0, 0, 0).strftime,
+          :end_at => DateTime.new(2013, 2, 25, 0, 0, 0, 0).strftime,
+          :value => {
+            :slug => "foo-bar",
+            :entries => 1000,
+            :successes => 500,
+            :format => "policy",
+          },
+        },
+      }
+    end
+
+    it "should insert a new item" do
+      ContentEngagementVisits.update_from_message(@message)
+      ContentEngagementVisits.all.should have(1).item
+
+      content_engagement_visits = ContentEngagementVisits.first
+      content_engagement_visits.slug.should == "foo-bar"
+      content_engagement_visits.source.should == "Google Analytics"
+      content_engagement_visits.entries.should == 1000
+      content_engagement_visits.successes.should == 500
+      content_engagement_visits.format.should == "policy"
+      content_engagement_visits.start_at.should == DateTime.new(2013, 2, 18, 0, 0, 0)
+      content_engagement_visits.end_at.should == DateTime.new(2013, 2, 25, 0, 0, 0)
+    end
+
+    it "should update an existing item" do
+      FactoryGirl.create(:content_engagement_visits,
+        :slug => "foo-bar",
+        :format => "policy",
+        :start_at => DateTime.new(2013, 2, 18, 0, 0, 0).strftime,
+        :end_at => DateTime.new(2013, 2, 25, 0, 0, 0).strftime,
+        :entries => 0,
+        :successes => 0,
+      )
+
+      ContentEngagementVisits.update_from_message(@message)
+      ContentEngagementVisits.all.should have(1).item
+
+      content_engagement_visits = ContentEngagementVisits.first
+      content_engagement_visits.entries.should == 1000
+      content_engagement_visits.successes.should == 500
+    end
+
+    it "should downcase the slug" do
+      @message[:payload][:value][:slug] = "FOO-BAR"
+
+      ContentEngagementVisits.update_from_message(@message)
+
+      content_engagement_visits = ContentEngagementVisits.first
+      content_engagement_visits.slug.should == "foo-bar"
     end
   end
 
