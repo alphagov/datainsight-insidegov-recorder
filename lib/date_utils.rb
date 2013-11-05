098385901a78d24ba5dc259e996f73dd291ece36
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-11-05 16:53:47 +0000

    Add method to localise dates for DST support
    Dates are local to Europe/London now (since integrating
    datainsight_recorder v0.4.1) and as such, local datetime should be used
    when querying as well.

--- lib/date_utils.rb
+++ lib/date_utils.rb
@@ -1,3 +1,5 @@
+require "tzinfo"
+
 module DateUtils
   def self.last_sunday_for(date_time)
     date_time - date_time.wday
@@ -9,5 +11,15 @@ module DateUtils
 
   def self.saturday_before(date_time)
     date_time - (date_time.wday + 1)
+  end
+
+  def self.localise(date_time)
+    # Convert a DateTime to the same time but in the Europe/London timezone
+    # ie. 2012-08-08T00:00:00+00:00 -> 2012-08-08T00:00:00+01:00
+    tz = TZInfo::Timezone.get("Europe/London")
+    if tz.period_for_utc(date_time).utc_total_offset_rational > 0
+      date_time = date_time.new_offset(tz.period_for_utc(date_time).utc_total_offset_rational) - Rational(1, 24)
+    end
+    date_time
   end
 end
